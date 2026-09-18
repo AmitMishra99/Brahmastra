031bc5179e38d468d5a196133cdfe890d301cb4d
Amit Mishra <aamitmishra364@gmail.com>
2026-09-18 16:45:33 +0530

    agents logo added

--- frontend/src/components/ChatArea/ChatInput.jsx
+++ frontend/src/components/ChatArea/ChatInput.jsx
@@ -11,9 +11,12 @@ import {
   setSelectedConversation,
 } from "../../redux/conversationSlice";
 import { updateConversationTitle } from "../../apis/updateConversationTitle";
+import { agents } from "../../utils/agents";
 
 const ChatInput = () => {
   const [value, setValue] = useState("");
+  const [selectedAgent, setSelectedAgent] = useState("Auto");
+
   const { selectedConversation, isNewConversation } = useSelector(
     (state) => state.conversation,
   );
@@ -61,12 +64,14 @@ const ChatInput = () => {
     const data = await createMessage({
       prompt,
       conversationId: conversation._id,
+      agent: selectedAgent.toLocaleLowerCase(),
     });
 
     dispatch(
       addMessage({
         role: "assistant",
-        content: data.response,
+        content: data?.response,
+        images: data?.images,
       }),
     );
   };
@@ -74,6 +79,29 @@ const ChatInput = () => {
   return (
     <div className="w-full overflow-hidden px-3 md:px-5 py-4 border-t border-white/[0.06] bg-[#0d0f14]">
       <div className="flex flex-col gap-2 bg-white/[0.03] border border-white/[0.07] rounded-2xl px-4 pt-3.5 pb-3 ">
+        <div className="flex w-[80%] gap-2 pr-2 flex-wrap">
+          {agents.map((agent) => {
+            const isActive = selectedAgent === agent.label;
+            const Icon = agent.icon;
+            return (
+              <div
+                key={agent.id}
+                onClick={() => setSelectedAgent(agent.label)}
+                className={`flex-shrink-0 cursor-pointer inline-flex items-center gap-1.5 px-3 py-2 rounded-full text-xs font-medium border transition-all ${
+                  isActive
+                    ? "bg-linear-to-r from-indigo-500 to-violet-600 text-white border-transparent shadow-[0_1px_8px_rgba(99,102,241,.35)] "
+                    : "bg-white/[0.03] text-slate-400 border-white/[0.06] hover:bg-white/[0.07]"
+                }`}
+              >
+                <Icon
+                  size={14}
+                  className={isActive ? "text-white" : "text-slate-500"}
+                />
+                {agent.label}
+              </div>
+            );
+          })}
+        </div>
         <textarea
           rows={3}
           onChange={(e) => setValue(e.target.value)}
